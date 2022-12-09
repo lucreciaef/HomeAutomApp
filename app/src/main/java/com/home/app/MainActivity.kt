1b3f6924f79fbbcbc28f894f91acdfd8e6d78541
lucreciaef <lucrecia.e.f@gmail.com>
2022-12-09 23:54:03 +0100

    merge functions on/off into one pass plug status as parameter simplify message displayed

--- app/src/main/java/com/home/app/MainActivity.kt
+++ app/src/main/java/com/home/app/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
 import com.home.app.databinding.ActivityMainBinding
 import com.home.app.ui.main.SectionsPagerAdapter
 import java.io.BufferedReader
@@ -24,6 +23,31 @@ class MainActivity : AppCompatActivity() {
 
     private var buttonPowerSwitchStatus = ButtonPowerSwitchStatus.POWER_IS_OFF
 
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+//        val viewPager: ViewPager = binding.viewPager
+//        viewPager.adapter = sectionsPagerAdapter
+//
+//        val buttonPowerSwitch: FloatingActionButton = binding.buttonPowerSwitch
+//
+//        buttonPowerSwitch.setOnClickListener { view ->
+//            val message = if (buttonPowerSwitchStatus == ButtonPowerSwitchStatus.POWER_IS_OFF) {
+//                turnOnShellyPlugExtractorFan()
+//                "uwu You turned me on"
+//            } else {
+////                turnOffShellyPlugExtractorFan()
+//                "uwu You turned me off"
+//            }
+//            Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+//                .setAction("Dismiss", null).show()
+//        }
+//    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -37,57 +61,25 @@ class MainActivity : AppCompatActivity() {
         val buttonPowerSwitch: FloatingActionButton = binding.buttonPowerSwitch
 
         buttonPowerSwitch.setOnClickListener { view ->
-            if (buttonPowerSwitchStatus==ButtonPowerSwitchStatus.POWER_IS_OFF) {
-                turnOnShellyPlugExtractorFan()
-                Snackbar.make(view, "uwu You turned me on", Snackbar.LENGTH_LONG)
-                    .setAction("Dismiss", null).show()
-            }
-            else if (buttonPowerSwitchStatus==ButtonPowerSwitchStatus.POWER_IS_ON) {
-                turnOffShellyPlugExtractorFan()
-                Snackbar.make(view, "uwu You turned me off", Snackbar.LENGTH_LONG)
-                    .setAction("Dismiss", null).show()
-            }
+            val status = if (buttonPowerSwitchStatus == ButtonPowerSwitchStatus.POWER_IS_OFF)
+                "on" else "off"
+
+            val message = if (status == "on") "uwu You turned me on" else "uwu You turned me off"
+
+            togglePowerShellyPlugExtractorFan(status)
+            Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+                .setAction("Dismiss", null).show()
         }
     }
 
-    private fun turnOnShellyPlugExtractorFan() {
-
+    private fun togglePowerShellyPlugExtractorFan(status: String) {
         val thread = Thread {
             try {
-                val requestURL = URL("http://192.168.0.111/relay/0?turn=on")
-
-                    with(requestURL.openConnection() as HttpURLConnection) {
-                        requestMethod = "POST"
-
-                        println("URL : $url")
-                        println("Response Code : $responseCode")
-
-                        BufferedReader(InputStreamReader(inputStream)).use {
-                            val response = StringBuffer()
-
-                            var inputLine = it.readLine()
-                            while (inputLine != null) {
-                                response.append(inputLine)
-                                inputLine = it.readLine()
-                            }
-                            println("Response : $response")
-                            buttonPowerSwitchStatus = ButtonPowerSwitchStatus.POWER_IS_ON
-                        }
-                    }
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-        thread.start()
-    }
-
-    private fun turnOffShellyPlugExtractorFan() {
-        val thread = Thread {
-            try {
-                val requestURL = URL("http://192.168.0.111/relay/0?turn=off")
+                val requestURL = URL("http://192.168.0.111/relay/0?turn=$status")
+                val httpMethod = "POST"
 
                 with(requestURL.openConnection() as HttpURLConnection) {
-                    requestMethod = "POST"
+                    requestMethod = httpMethod
 
                     println("URL : $url")
                     println("Response Code : $responseCode")
@@ -101,7 +93,9 @@ class MainActivity : AppCompatActivity() {
                             inputLine = it.readLine()
                         }
                         println("Response : $response")
-                        buttonPowerSwitchStatus = ButtonPowerSwitchStatus.POWER_IS_OFF
+                        buttonPowerSwitchStatus =
+                            if (status == "on") ButtonPowerSwitchStatus.POWER_IS_ON
+                            else ButtonPowerSwitchStatus.POWER_IS_OFF
                     }
                 }
             } catch (e: Exception) {
@@ -110,4 +104,65 @@ class MainActivity : AppCompatActivity() {
         }
         thread.start()
     }
+
+//    private fun turnOnShellyPlugExtractorFan() {
+//
+//        val thread = Thread {
+//            try {
+//                val requestURL = URL("http://192.168.0.111/relay/0?turn=on")
+//
+//                with(requestURL.openConnection() as HttpURLConnection) {
+//                    requestMethod = "POST"
+//
+//                    println("URL : $url")
+//                    println("Response Code : $responseCode")
+//
+//                    BufferedReader(InputStreamReader(inputStream)).use {
+//                        val response = StringBuffer()
+//
+//                        var inputLine = it.readLine()
+//                        while (inputLine != null) {
+//                            response.append(inputLine)
+//                            inputLine = it.readLine()
+//                        }
+//                        println("Response : $response")
+//                        buttonPowerSwitchStatus = ButtonPowerSwitchStatus.POWER_IS_ON
+//                    }
+//                }
+//            } catch (e: Exception) {
+//                e.printStackTrace()
+//            }
+//        }
+//        thread.start()
+//    }
+//
+//    private fun turnOffShellyPlugExtractorFan() {
+//        val thread = Thread {
+//            try {
+//                val requestURL = URL("http://192.168.0.111/relay/0?turn=off")
+//
+//                with(requestURL.openConnection() as HttpURLConnection) {
+//                    requestMethod = "POST"
+//
+//                    println("URL : $url")
+//                    println("Response Code : $responseCode")
+//
+//                    BufferedReader(InputStreamReader(inputStream)).use {
+//                        val response = StringBuffer()
+//
+//                        var inputLine = it.readLine()
+//                        while (inputLine != null) {
+//                            response.append(inputLine)
+//                            inputLine = it.readLine()
+//                        }
+//                        println("Response : $response")
+//                        buttonPowerSwitchStatus = ButtonPowerSwitchStatus.POWER_IS_OFF
+//                    }
+//                }
+//            } catch (e: Exception) {
+//                e.printStackTrace()
+//            }
+//        }
+//        thread.start()
+//    }
 }
