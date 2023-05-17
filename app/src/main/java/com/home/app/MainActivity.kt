0026de0ebb00cb65361adb881c1d13771ebb3070
lucreciaef <lucrecia.e.f@gmail.com>
2023-05-17 21:53:51 +0200

    moves button from MainActivity to Fragment

--- app/src/main/java/com/home/app/MainActivity.kt
+++ app/src/main/java/com/home/app/MainActivity.kt
@@ -3,73 +3,13 @@ package com.home.app
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.gson.Gson
 import com.home.app.databinding.ActivityMainBinding
 import com.home.app.ui.main.SectionsPagerAdapter
-import kotlinx.coroutines.*
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
-import kotlin.concurrent.thread
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
-    enum class ButtonPowerSwitchStatus {
-        POWER_IS_ON, POWER_IS_OFF
-    }
-
-    private var buttonPowerSwitchStatus = ButtonPowerSwitchStatus.POWER_IS_OFF
-
-    private fun getInitialButtonPowerSwitchStatus() {
-        thread(true) {
-            try {
-                val requestURL = URL("http://192.168.0.111/relay/0")
-                val httpMethod = "GET"
-
-                with(requestURL.openConnection() as HttpURLConnection) {
-                    requestMethod = httpMethod
-
-                    println("URL : $url")
-                    println("Response Code : $responseCode")
-
-                    BufferedReader(InputStreamReader(inputStream)).use {
-                        val response = StringBuffer()
-
-                        var inputLine = it.readLine()
-                        while (inputLine != null) {
-                            response.append(inputLine)
-                            inputLine = it.readLine()
-                        }
-                        println("Response : $response")
-                        // Parse the JSON response to retrieve the initial status of the power switch
-                        val gson = Gson()
-                        val responseJson =
-                            gson.fromJson(response.toString(), ShellyPlugJSON::class.java)
-                        val initialStatus = responseJson.ison.toString()
-//                        onResponseHandler(initialStatus)
-
-                        buttonPowerSwitchStatus =
-                            if (initialStatus == "false") ButtonPowerSwitchStatus.POWER_IS_OFF
-                            else ButtonPowerSwitchStatus.POWER_IS_ON
-
-                        // Show a snackbar message with the initial power switch status
-                        Snackbar.make(binding.root, "Is the switch ON?: $initialStatus", Snackbar.LENGTH_LONG)
-                            .setAction("Dismiss", null).show()
-                    }
-                }
-            } catch (e: Exception) {
-                //                e.printStackTrace()
-                Snackbar.make(binding.root, "Could not connect", Snackbar.LENGTH_LONG)
-                    .setAction("Dismiss", null).show()
-            }
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -77,57 +17,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Call the getInitialButtonPowerSwitchStatus function and pass the callback function as an argument
-        getInitialButtonPowerSwitchStatus()
-
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = binding.viewPager
         viewPager.adapter = sectionsPagerAdapter
-
-        val buttonPowerSwitch: FloatingActionButton = binding.buttonPowerSwitch
-
-        buttonPowerSwitch.setOnClickListener { view ->
-            val status = if (buttonPowerSwitchStatus == ButtonPowerSwitchStatus.POWER_IS_OFF)
-                "on" else "off"
-
-            val message = if (status == "on") "uwu You turned me on" else "uwu You turned me off"
-
-            togglePowerShellyPlugExtractorFan(status)
-            Snackbar.make(view, message, Snackbar.LENGTH_LONG)
-                .setAction("Dismiss", null).show()
-        }
-    }
-
-    private fun togglePowerShellyPlugExtractorFan(status: String) {
-        thread(true) {
-            try {
-                val requestURL = URL("http://192.168.0.111/relay/0?turn=$status")
-                val httpMethod = "POST"
-
-                with(requestURL.openConnection() as HttpURLConnection) {
-                    requestMethod = httpMethod
-
-                    println("URL : $url")
-                    println("Response Code : $responseCode")
-
-                    BufferedReader(InputStreamReader(inputStream)).use {
-                        val response = StringBuffer()
-
-                        var inputLine = it.readLine()
-                        while (inputLine != null) {
-                            response.append(inputLine)
-                            inputLine = it.readLine()
-                        }
-                        println("Response : $response")
-                        buttonPowerSwitchStatus =
-                            if (status == "on") ButtonPowerSwitchStatus.POWER_IS_ON
-                            else ButtonPowerSwitchStatus.POWER_IS_OFF
-                    }
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
     }
 }
 
--- app/src/main/java/com/home/app/ui/main/PlaceholderFragment.kt
+++ app/src/main/java/com/home/app/ui/main/PlaceholderFragment.kt
@@ -6,10 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
+//import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.home.app.R
 import com.home.app.databinding.FragmentMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.gson.Gson
+import com.home.app.ShellyPlugJSON
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 /**
  * A placeholder fragment containing a simple view.
@@ -23,6 +31,89 @@ class PlaceholderFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    enum class ButtonPowerSwitchStatus {
+        POWER_IS_ON, POWER_IS_OFF
+    }
+
+    private var buttonPowerSwitchStatus = ButtonPowerSwitchStatus.POWER_IS_OFF
+
+    private fun getInitialButtonPowerSwitchStatus() {
+        thread(true) {
+            try {
+                val requestURL = URL("http://192.168.0.111/relay/0")
+                val httpMethod = "GET"
+
+                with(requestURL.openConnection() as HttpURLConnection) {
+                    requestMethod = httpMethod
+
+                    println("URL : $url")
+                    println("Response Code : $responseCode")
+
+                    BufferedReader(InputStreamReader(inputStream)).use {
+                        val response = StringBuffer()
+
+                        var inputLine = it.readLine()
+                        while (inputLine != null) {
+                            response.append(inputLine)
+                            inputLine = it.readLine()
+                        }
+                        println("Response : $response")
+                        // Parse the JSON response to retrieve the initial status of the power switch
+                        val gson = Gson()
+                        val responseJson =
+                            gson.fromJson(response.toString(), ShellyPlugJSON::class.java)
+                        val initialStatus = responseJson.ison.toString()
+//                        onResponseHandler(initialStatus)
+
+                        buttonPowerSwitchStatus =
+                            if (initialStatus == "false") ButtonPowerSwitchStatus.POWER_IS_OFF
+                            else ButtonPowerSwitchStatus.POWER_IS_ON
+
+                        // Show a snack-bar message with the initial power switch status
+                        Snackbar.make(binding.root, "Is the switch ON?: $initialStatus", Snackbar.LENGTH_LONG)
+                            .setAction("Dismiss", null).show()
+                    }
+                }
+            } catch (e: Exception) {
+                //                e.printStackTrace()
+                Snackbar.make(binding.root, "Could not connect", Snackbar.LENGTH_LONG)
+                    .setAction("Dismiss", null).show()
+            }
+        }
+    }
+
+    private fun togglePowerShellyPlugExtractorFan(status: String) {
+        thread(true) {
+            try {
+                val requestURL = URL("http://192.168.0.111/relay/0?turn=$status")
+                val httpMethod = "POST"
+
+                with(requestURL.openConnection() as HttpURLConnection) {
+                    requestMethod = httpMethod
+
+                    println("URL : $url")
+                    println("Response Code : $responseCode")
+
+                    BufferedReader(InputStreamReader(inputStream)).use {
+                        val response = StringBuffer()
+
+                        var inputLine = it.readLine()
+                        while (inputLine != null) {
+                            response.append(inputLine)
+                            inputLine = it.readLine()
+                        }
+                        println("Response : $response")
+                        buttonPowerSwitchStatus =
+                            if (status == "on") ButtonPowerSwitchStatus.POWER_IS_ON
+                            else ButtonPowerSwitchStatus.POWER_IS_OFF
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         pageViewModel = ViewModelProvider(this).get(PageViewModel::class.java).apply {
@@ -33,15 +124,31 @@ class PlaceholderFragment : Fragment() {
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+
+        // Call the getInitialButtonPowerSwitchStatus function and pass the callback function as an argument
+        getInitialButtonPowerSwitchStatus()
 
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         val root = binding.root
 
+        val buttonPowerSwitch: FloatingActionButton = binding.buttonPowerSwitch
+
+        buttonPowerSwitch.setOnClickListener { view ->
+            val status = if (buttonPowerSwitchStatus == ButtonPowerSwitchStatus.POWER_IS_OFF)
+                "on" else "off"
+
+            val message = if (status == "on") "uwu You turned me on" else "uwu You turned me off"
+
+            togglePowerShellyPlugExtractorFan(status)
+            Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+                .setAction("Dismiss", null).show()
+        }
+
         val textView: TextView = binding.sectionLabel
-        pageViewModel.text.observe(viewLifecycleOwner, Observer {
+        pageViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
-        })
+        }
         return root
     }
 
@@ -71,3 +178,4 @@ class PlaceholderFragment : Fragment() {
         _binding = null
     }
 }
+
